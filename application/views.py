471448008c4709afee546b7254b4cd3b69af4354
Tom Øyvind Hogstad <tom.oyvind.hogstad@ctfk.no>
2020-03-26 10:31:06 +0100

    La inn form post template

--- application/views.py
+++ application/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from flask import Flask, render_template, request
 from . import app
 
@@ -7,11 +8,14 @@ def index():
 
 @app.route("/form")
 def form():
-    return render_template('form.html')
+    return render_template('form.html', dato=date.today())
 
 @app.route("/form/post", methods=['POST'])
 def form_post():
-    return '<pre>Fornavn: {}\nEtternavn: {}\nDato:{}</pre>'.format(
-        request.form['firstname'], 
-        request.form['lastname'], 
-        request.form['dato'])
+    return render_template('form_post.html', 
+        firstname=request.form['firstname'],
+        lastname=request.form['lastname'],
+        dato=request.form['dato'],
+        # Bruker for..get() for å få en "default" verdi der det ikke er noen verdi
+        ok=request.form.get('ok'),
+    )
