88ccce099ef4c72d5a35cf40e5dccd60baaeb5aa
Tom Øyvind Hogstad <tom.oyvind.hogstad@ctfk.no>
2020-04-02 22:39:42 +0200

    Komma, punktum problematikk

--- application/form.py
+++ application/form.py
@@ -17,3 +17,18 @@ class MeasurementForm(FlaskForm):
     registered = DateTimeField('Dato/Tid', default=datetime.now(), validators=[DataRequired()])
     bmp280_temperature = FloatField('BME280 Temperatur', validators=[DataRequired()])
     bmp280_pressure = FloatField('BME280 Trykk', validators=[DataRequired()])
+
+
+
+'''
+# Hvordan få flyttall feilt til å akseptere komma
+# Se eventuelt på use_locale parameter og installere Babel pakken og DecimalField
+class MyFloatField(FloatField):
+    def process_formdata(self, valuelist):
+        if valuelist:
+            try:
+                self.data = float(valuelist[0].replace(',', '.'))
+            except ValueError:
+                self.data = None
+                raise ValueError(self.gettext('Not a valid float value'))
+'''
