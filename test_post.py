01779a6a08fff552c1a72726c1d7a2ae6003f02e
Tom Øyvind Hogstad <tom.oyvind.hogstad@vtfk.no>
2020-04-16 11:16:36 +0200

    Endringer 16.04.2020

---
+++ test_post.py
@@ -0,0 +1,19 @@
+import requests
+from random import randint
+from datetime import datetime
+
+
+for n in range(100):
+    data = {}
+    data['unit_id'] = 1
+    #data['registered'] = str(datetime.now())
+    dt = datetime.now()
+    dt = dt.replace(day=randint(1,30))
+    # TODO: Utvid med feltene til sensorene deres
+    data['registered'] = str(dt)    
+    data['bmp280_temperature'] = randint(20,30)
+    data['bmp280_pressure'] = randint(950, 1100)
+    #print('Data som sendes til Flask App:', data)
+    r = requests.post('http://127.0.0.1:5000/api/post_data', json=data)
+    print(r.text)
+
