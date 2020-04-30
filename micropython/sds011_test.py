c2c46efc82727489f7423a3e51ccdcf18eda831a
Tom Øyvind Hogstad <tom.oyvind.hogstad@vtfk.no>
2020-04-30 13:37:00 +0200

    sds011 fungerende test

--- micropython/sds011_test.py
+++ micropython/sds011_test.py
@@ -1,11 +1,11 @@
 from machine import UART, sleep
 import sds011
-uart = UART(1)
-uart.init(9600, tx=17, rx=16)
+uart = UART(1, baudrate=9600, bits=8, parity=None, stop=1, tx=12, rx=14, rts=-1, cts=-1, txbuf=256, rxbuf=256, timeout=5000, timeout_char=2)
+
 dust_sensor = sds011.SDS011(uart)
 while True:
     if dust_sensor.read():
         print('pm2.5', dust_sensor.pm25, 'pm10', dust_sensor.pm10)
     else:
         print('No read')
-    sleep(5000)
+    sleep(2000)
